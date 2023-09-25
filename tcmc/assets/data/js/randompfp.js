71b172483e1608cdbb3a7a7a74b68f3a6451ca06
TheCeruleanSnak3 <54421333+CeruleanSnake@users.noreply.github.com>
2023-09-25 23:20:35 +1000

    Code Updates and additional Additions

--- tcmc/assets/data/js/randompfp.js
+++ tcmc/assets/data/js/randompfp.js
@@ -1,6 +1,8 @@
  var random_images_array = ['random1.png', 'random2.png', 'random3.png', 'random4.png'];
 
- function getRandomImage(imgAr, path) {
+document.getElementsByClassName("randompfp");
+getRandomImage(random_images_array, '../img/pfp/')
+function getRandomImage(imgAr,path) {
      path = path || './assets/img/pfp/';
      var num = Math.floor(Math.random() * imgAr.length);
      var img = imgAr[num];
@@ -8,4 +10,9 @@
          '">';
      document.write(imgStr);
      document.close();
- }
+}
+ 
+MSPFA.slide.push(p => {
+    document.getElementsByClassName("randompfp").innerHTML = 
+    "<script>getRandomImage(random_images_array, '../img/pfp/')</script>"
+});
