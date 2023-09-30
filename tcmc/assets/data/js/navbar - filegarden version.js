6af55851b4504d07174f3c157f8ec7952625a082
TheCeruleanSnak3 <54421333+CeruleanSnake@users.noreply.github.com>
2023-09-30 17:29:38 +1000

    Character profile update

--- tcmc/assets/data/js/navbar - filegarden version.js
+++ tcmc/assets/data/js/navbar - filegarden version.js
@@ -1,2 +1,2 @@
 document.getElementById("nav").innerHTML =
-    "</span> <a href=\"https://mspfa.com/?s=38151&p=1\">MSPFA</a> <div class=\"heart\"></div> </div> <a href=\"./Extras.html\">Extras</a> <span class=\"vbar\"> | </span> <a href=\"./Credits.html\">Credits</a> <span class=\"vbar\"> | </span> <a href=\"https://twitter.com/CeruleaSnake\">Twitter</a> <div class=\"heart\"></div> <a href=\"./map.html\">Map</a>";
+    "</span> <a href=\"https://mspfa.com/?s=38151&p=1\">MSPFA</a> <div class=\"heart\"></div> </div> <a href=\"./Extras.html\">Extras</a> <span class=\"vbar\"> | </span> <a href=\"./Credits.html\">Credits</a> <span class=\"vbar\"> | </span> <a href=\"https://twitter.com/CeruleaSnake\">Twitter</a> <div class=\"heart\"></div> <a href=\"./Map.html\">Map</a>";
