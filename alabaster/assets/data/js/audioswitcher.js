268366d2f2b4a4f81a50b032545e476e63c0ed4b
TheCeruleanSnak3 <54421333+CeruleanSnake@users.noreply.github.com>
2023-10-17 09:05:35 +1100

    Alabaster Updating

--- alabaster/assets/data/js/audioswitcher.js
+++ alabaster/assets/data/js/audioswitcher.js
@@ -1,4 +1,4 @@
-console.log("Audio Script by Steelo");
+console.log("#####| Audio Script by Steelo |#####");
 
 var is_in_range = (num, track) => {
   return num >= track[0] && num <= track[1];
@@ -7,123 +7,118 @@ var is_in_range = (num, track) => {
 const audio_data = [
   [
     [
-      [404, 410]
+      [458, 473]
     ], new Audio('./assets/sound/001_conquest_of_paradise_hiphop.mp3')
   ],
   [
     [
-      [665, 680]
-    ], new Audio('./assets/sound/001_conquest_of_paradise_hiphop.mp3')
-  ],
-  [
-    [
-      [729, 732]
+      [732, 747]
     ], new Audio('./assets/sound/002_vangelis_antarctica.mp3')
   ],
   [
     [
-      [733, 741]
+      [798, 804]
     ], new Audio('./assets/sound/003_scharvona_ice_for_life.mp3')
   ],
   [
     [
-      [753, 833]
+      [805, 813]
     ], new Audio('./assets/sound/004_scharvona_be_wheely_careful.mp3')
   ],
   [
     [
-      [864, 892]
+      [825, 863]
     ], new Audio('./assets/sound/005_sanoandaihara_twelfth_chapter_final.mp3')
   ],
   [
     [
-      [864, 892],
-      [1725, 1748]
+      [936, 964],
+      [1840, 1858]
     ], new Audio('./assets/sound/006_monaca_and_nishimura_godsboundsbyrules.mp3')
   ],
   [
     [
-      [909, 916]
+      [981, 988]
     ], new Audio('./assets/sound/007_yamaoka_ice.mp3')
   ],
   [
     [
-      [990, 1010]
+      [1062, 1087]
     ], new Audio('./assets/sound/008_yamaoka_devils_laughter.mp3')
   ],
   [
     [
-      [1132, 1144]
+      [1209, 1221]
     ], new Audio('./assets/sound/009_scharvona_say_no_to_snow.mp3')
   ],
   [
     [
-      [1281, 1310]
+      [1376, 1405]
     ], new Audio('./assets/sound/010_yamaoka_fermata_in_mistic_air.mp3')
   ],
   [
     [
-      [1316, 1318]
+      [1411, 1413]
     ], new Audio('./assets/sound/011_morricone_contamination.mp3')
   ],
   [
     [
-      [1439, 1452]
+      [1551, 1564]
     ], new Audio('./assets/sound/012_ito_grand_papillon.mp3')
   ],
   [
     [
-      [1512, 1512]
+      [1624, 1624]
     ], new Audio('./assets/sound/013_wallscoveredinblood_cattetremix.mp3')
   ],
   [
     [
-      [1527, 1540]
+      [1639, 1652]
     ], new Audio('./assets/sound/014_vangelis_eternity.mp3')
   ],
   [
     [
-      [1621, 1627]
+      [1733, 1739]
     ], new Audio('./assets/sound/015_henifin_the_gift_of_forever.mp3')
   ],
   [
     [
-      [1780, 1786]
+      [1895, 1906]
     ], new Audio('./assets/sound/016_futureworldmusic_life_goes_on.mp3')
   ],
   [
     [
-      [1787, 1826]
+      [1907, 1946]
     ], new Audio('./assets/sound/017_henifin_the_somme.mp3')
   ],
   [
     [
-      [1883, 1888]
+      [2003, 2008]
     ], new Audio('./assets/sound/018_yamaoka_butterflies.mp3')
   ],
   [
     [
-      [1998, 2016]
+      [2118, 2136]
     ], new Audio('./assets/sound/019_henifin_a_war_to_end_all_wars.mp3')
   ],
   [
     [
-      [2019, 2062]
+      [2139, 2182]
     ], new Audio('./assets/sound/020_henifin_gateway_to_destiny.mp3')
   ],
   [
     [
-      [2140, 2190]
+      [2260, 2310]
     ], new Audio('./assets/sound/021_harland_hedges_and_peasee_kain_battle_theme.mp3')
   ],
   [
     [
-      [2318, 2361]
+      [2438, 2481]
     ], new Audio('./assets/sound/022_morricone_eternity.mp3')
   ],
   [
     [
-      [2525, 2544]
+      [2655, 2674]
     ], new Audio('./assets/sound/023_vangelis_titans.mp3')
   ],
 ];
