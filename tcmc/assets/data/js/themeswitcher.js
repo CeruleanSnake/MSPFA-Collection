ec01aa034549c12e167a0df9beb9d34fab111e93
TheCeruleanSnak3 <54421333+CeruleanSnake@users.noreply.github.com>
2023-10-25 08:04:36 +1100

    Update

--- tcmc/assets/data/js/themeswitcher.js
+++ tcmc/assets/data/js/themeswitcher.js
@@ -4,7 +4,7 @@ MSPFA.slide.push(() => {
     if ([1].includes(parseInt(params.get("p")))) {
       document.getElementById("sitetheme").innerHTML = "<link rel='stylesheet' href='./assets/data/theme/myutun-sea.css'>";
     }
-    if ([2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 328].includes(parseInt(params.get("p")))) {
+    if ([2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 328, 329].includes(parseInt(params.get("p")))) {
       document.getElementById("sitetheme").innerHTML = "<link rel='stylesheet' href='./assets/data/theme/alternia.css'>";
     }
     if ([16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 113, 114, 115, 116, 117, 118, 119, 142, 143, 144, 145, 146, 204, 205, 206, 207].includes(parseInt(params.get("p")))) {
