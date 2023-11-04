c23adc82c42da46d78074db5d4d16e41937f6d6e
TheCeruleanSnak3 <54421333+CeruleanSnake@users.noreply.github.com>
2023-11-05 00:03:49 +1100

    Alabaster extended universe addition

---
+++ tcmc/assets/data/js/fonttoggle.js
@@ -0,0 +1,31 @@
+// Font enable/disable checkbox
+
+const fontLabel = document.createElement('label');
+const fontInput = document.createElement('input');
+fontInput.type = 'checkbox';
+fontInput.checked = true;
+fontLabel.appendChild(document.createTextNode('Enable custom font: '));
+fontLabel.appendChild(fontInput);
+
+const adventureInfo = document.querySelector('#infobox table tr:last-child span');
+adventureInfo.appendChild(document.createElement('br'));
+adventureInfo.appendChild(document.createElement('br'));
+adventureInfo.appendChild(fontLabel);
+
+const fontStyle = document.querySelector('#font');
+const styleParent = fontStyle.parentNode;
+
+if (localStorage.customFont !== undefined && localStorage.customFont == "false") {
+    styleParent.removeChild(fontStyle);
+    fontInput.checked = false;
+}
+
+fontInput.addEventListener('change', () => {
+    localStorage.customFont = fontInput.checked;
+
+    if (localStorage.customFont !== "false") {
+        styleParent.appendChild(fontStyle);
+    } else {
+        styleParent.removeChild(fontStyle);
+    }
+});
